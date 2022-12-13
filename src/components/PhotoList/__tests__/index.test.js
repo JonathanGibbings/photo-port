f8597e1a99e1a5361718123777094b248de4b367
JGibbings <j_gibbings@msn.com>
2022-12-13 14:36:48 -0500

    add tests for contactform

--- src/components/PhotoList/__tests__/index.test.js
+++ src/components/PhotoList/__tests__/index.test.js
@@ -1,18 +1,18 @@
 // __tests__/PhotoList.test.js
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import PhotoList from "../";
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import PhotoList from './PhotoList'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
-describe("PhotoList is rendering", () => {
-  it("renders", () => {
+describe('PhotoList is rendering', () => {
+  it('renders', () => {
     render(<PhotoList />);
   });
 
-  it("renders snapshot", () => {
-    const { asFragment } = render(<PhotoList />);
-    expect(asFragment()).toMatchSnapshot();
+  it('renders', () => {
+    const { asFragment } = render(<PhotoList />)
+    expect(asFragment()).toMatchSnapshot()
   });
 });
